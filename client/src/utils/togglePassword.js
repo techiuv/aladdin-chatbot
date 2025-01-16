b4f234bde880ce110764db7bf4a2a23717566b19
Yuvraj <mail.yuvraj0317@gmaiil.com>
2025-01-16 13:30:54 +0530

    resolve conflicts

--- client/src/utils/togglePassword.js
+++ client/src/utils/togglePassword.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const togglePasswordVisibility = (inputId) => {
     const inputElement = document.getElementById(inputId);
     if (inputElement) {
@@ -7,14 +6,4 @@ export const togglePasswordVisibility = (inputId) => {
       return newType; // Return the new type to update the UI
     }
   };
-=======
-export const togglePasswordVisibility = (inputId) => {
-    const inputElement = document.getElementById(inputId);
-    if (inputElement) {
-      const newType = inputElement.type === 'password' ? 'text' : 'password';
-      inputElement.type = newType;
-      return newType; // Return the new type to update the UI
-    }
-  };
->>>>>>> c9973c7f94f4d329365fbbb2db74be7bcabde679
   
--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -48,54 +47,3 @@ class Server:
 if __name__ == '__main__':
     server = Server()
     server.run()
-=======
-from flask import Flask
-from flask_cors import CORS
-from dotenv import load_dotenv
-from flask_jwt_extended import JWTManager
-import os
-
-# Import Blueprints
-from routes.user_route import auth_bp
-from routes.chats_routes import chat_bp
-from routes.admin_route import admin_bp
-
-# Load environment variables
-load_dotenv()
-
-
-class Server:
-
-
-    def __init__(self):
-    
-        self.app = Flask(__name__)
-        self.configure_app()
-        self.register_extensions()
-        self.register_blueprints()
-
-    def configure_app(self):
- 
-        self.app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
-        self.port = int(os.getenv('PORT', 5000))
-        CORS(self.app)
-
-    def register_extensions(self):
-
-        self.jwt = JWTManager(self.app)
-
-    def register_blueprints(self):
-
-        self.app.register_blueprint(auth_bp, url_prefix="/auth")
-        self.app.register_blueprint(chat_bp, url_prefix='/api')
-        self.app.register_blueprint(admin_bp, url_prefix='/admin')
-
-    def run(self):
-   
-        self.app.run(debug=True, port=self.port)
-
-
-if __name__ == '__main__':
-    server = Server()
-    server.run()
->>>>>>> c9973c7f94f4d329365fbbb2db74be7bcabde679
